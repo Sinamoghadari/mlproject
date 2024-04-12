84268c5406e4e352a3e1f1d4fbe5d7469bfcaf0e
Sinamoghadari <sinamoghadary@yahoo.com>
2024-04-12 18:03:19 +0330

    Data Transformation done

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -6,12 +6,14 @@ parent_directory = os.path.dirname(current_file)
 src_directory = os.path.abspath(os.path.join(parent_directory, os.pardir))
 sys.path.append(src_directory)
 
-
 from exception import CustomException
 from logger import logging
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+from data_transformation import DataTransformation
+from data_transformation import DataTransformationConfig
+
 
 @dataclass
 class DataIngestionConfig :
@@ -26,7 +28,7 @@ class DataIngestion :
     def initiate_data_ingestion(self) :
         logging.info('Entering the data ingestion method or component')
         try :
-            df = pd.read_csv('notebook\data\stud.csv')
+            df = pd.read_csv('/home/lenovo/anaconda3/envs/mlproject/notebook/data/stud.csv')
             logging.info('Read the dataset as dataframe')
 
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path) , exist_ok= True)
@@ -54,3 +56,6 @@ class DataIngestion :
 if __name__ == '__main__' :
     obj = DataIngestion()
     train_data , test_data = obj.initiate_data_ingestion()
+
+    data_transformation = DataTransformation()
+    data_transformation.initiate_data_transformation(train_data, test_data)
