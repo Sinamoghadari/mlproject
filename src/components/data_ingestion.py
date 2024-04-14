632519490c221fdc9fc185920033e465550830ff
Sina Moghadari <sinamoghadary@yahoo.com>
2024-04-14 12:19:25 +0330

    Model Trainer

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -13,6 +13,8 @@ from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 from data_transformation import DataTransformation
 from data_transformation import DataTransformationConfig
+from model_trainer import ModelTrainerConfig
+from model_trainer import ModelTrainer
 
 
 @dataclass
@@ -28,7 +30,7 @@ class DataIngestion :
     def initiate_data_ingestion(self) :
         logging.info('Entering the data ingestion method or component')
         try :
-            df = pd.read_csv('/home/lenovo/anaconda3/envs/mlproject/notebook/data/stud.csv')
+            df = pd.read_csv("G:/ML-Projects/mlproject/artifacts/data.csv")
             logging.info('Read the dataset as dataframe')
 
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path) , exist_ok= True)
@@ -58,4 +60,7 @@ if __name__ == '__main__' :
     train_data , test_data = obj.initiate_data_ingestion()
 
     data_transformation = DataTransformation()
-    data_transformation.initiate_data_transformation(train_data, test_data)
+    train_arr , test_arr , _= data_transformation.initiate_data_transformation(train_data, test_data)
+
+    model_trainer = ModelTrainer()
+    print(model_trainer.initiate_model_trainer(train_arr , test_arr))
