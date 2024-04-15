5bf3ac4261c0ef1923c10c2e14262993ac120eae
Sina Moghadari <sinamoghadary@yahoo.com>
2024-04-15 10:23:43 +0330

    Prediction Pipeline

---
+++ src/pipeline/predict_pipeline.py
@@ -0,0 +1,77 @@
+import sys
+import os
+
+current_file = os.path.abspath(__file__)
+parent_directory = os.path.dirname(current_file)
+src_directory = os.path.abspath(os.path.join(parent_directory, os.pardir))
+sys.path.append(src_directory)
+
+from exception import CustomException
+from utils import load_object
+import pandas as pd
+import numpy as np
+
+class PredictPipeline :
+    def __int__(self) :
+        pass
+
+    def predict(self , features) :
+            try :
+
+                model_path = os.path.join('artifacts' , 'model.pkl')
+                preprocessor_path = os.path.join('artifacts' , 'preprocessor.pkl')
+
+                model = load_object(model_path)
+                preprocessor = load_object(preprocessor_path)
+
+                data_scaled = preprocessor.transform(features)
+                prediction = model.predict(data_scaled)
+
+                return prediction
+            
+            except Exception as e :
+                raise CustomException(e , sys)    
+
+
+class CustomData :
+    def __init__(self ,
+                gender : str ,
+                race_ethnicity : str ,
+                parental_level_of_education ,
+                lunch : str ,
+                test_preparation_course : str ,
+                reading_score : int ,
+                writing_score : int
+                 ) :
+        self.gender = gender
+
+        self.race_ethnicity = race_ethnicity
+
+        self.parental_level_of_education = parental_level_of_education
+
+        self.lunch = lunch
+
+        self.test_preparation_course = test_preparation_course
+
+        self.reading_score = reading_score
+
+        self.writing_score = writing_score
+
+
+    def get_data_as_data_frame(self) :
+        try :
+            custom_data_input_dict = {
+                'gender' : [self.gender] ,
+                'race_ethnicity' : [self.race_ethnicity] ,
+                'parental_level_of_education' : [self.parental_level_of_education] ,
+                'lunch' : [self.lunch] , 
+                'test_preparation_course' : [self.test_preparation_course] ,
+                'reading_score' : [self.reading_score] ,
+                'writing_score' : [self.writing_score]
+            }
+
+            return pd.DataFrame(custom_data_input_dict)    
+    
+        except Exception as e:
+            raise CustomException(e , sys)
+        
